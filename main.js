43de85418326ad63bd1b862ad5e06ede4bf5758d
moonsunsong <18190821253@163.com>
2018-08-21 10:10:30 +0800

    项目重来

---
+++ main.js
@@ -0,0 +1,11 @@
+
+var num1 = document.getElementById("num1");
+var num2 = document.getElementById("num2");
+var num3 = document.getElementById("num3");
+var count = document.getElementById("count");
+
+count.onclick = function(){
+    var add = require('./calc.js');
+    num3.value = add(parseFloat(num1.value),parseFloat(num2.value));
+}
+    
---
+++ webpack.config.js
@@ -0,0 +1,10 @@
+
+
+module.exports={
+    entry:'./main.js',
+    output:{
+        path:__dirname+'/dist',
+        filename:'build.js'
+    },
+    mode:'production'
+}
